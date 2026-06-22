d573be0a0c31fe26c171eef4424839313e4344e7
Jun Yu <75609725+chuajunyu@users.noreply.github.com>
2026-06-22 22:29:30 +0800

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+
+        # keep a monotonically decreasing stack
+        # this is the same as the mountain stack problem!!!
+
+        result = [0] * len(temperatures)
+        stack = []
+        for i, t in enumerate(temperatures):
+            if not stack:
+                stack.append((i, t))
+            else:
+                while stack:
+                    x, top = stack[-1]
+                    if t > top:
+                        result[x] = i - x
+                        stack.pop()
+                    else:
+                        break
+                stack.append((i, t))
+        return result
+
